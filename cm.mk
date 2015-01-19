2f418dd8700bc3ff43239ec00d42b4bfbfd546ce
Zach Frank <zachf714@gmail.com>
2015-01-18 19:45:18 -0500

    Change to cm files
    Change-Id: I459c2d97172c537be1b84af61f5a0ac55e4ce8f9

---
+++ cm.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2011 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Low-FPS Bootanimation
+USE_LOWFPS_BOOTANI := true
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Inherit from those products. Most specific first.
+
+$(call inherit-product, device/asus/tf101/full_tf101.mk)
+
+# Configure as xhdpi device to prevent breaking without mdpi drawables
+PRODUCT_AAPT_CONFIG := normal mdpi hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := cm_tf101
+PRODUCT_DEVICE := tf101
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Transformer TF101
+PRODUCT_MANUFACTURER := asus
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=US_epad \
+    BUILD_FINGERPRINT="asus/US_epad/TF101:4.0.3/IML74K/US_epad-9.2.1.27-20120615:user/release-keys" \
+    PRIVATE_BUILD_DESC="US_epad-user 4.0.3 IML74K US_epad-9.2.1.27-20120615 release-keys"
+
+
